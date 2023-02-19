b0910ca62b84262b537d1c89894aeb552f1015ca
DiamondCoder1000 <78707555+CheatMaker@users.noreply.github.com>
2023-02-19 15:27:44 -0800

    e

--- Skidware/SDK/LocalPlayer.hpp
+++ Skidware/SDK/LocalPlayer.hpp
@@ -14,6 +14,7 @@ private:
 	jfieldID motionYfd = NULL;
 	jfieldID motionZfd = NULL;
 	jfieldID playerCapablitiesfd = NULL;
+	jfieldID hurttimefd = NULL;
 
 	jobject getNetHandlerPlayClientObject = NULL;
 	NetHandlerPlayClient NetHandlerPlayClientInstance = NULL;
